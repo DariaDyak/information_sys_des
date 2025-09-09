b1d5a919df365d814d21b47188463f4f1a85eb6f
DariaDyak <144004029+DariaDyak@users.noreply.github.com>
2025-09-09 22:23:34 +0300

    Задание 4. Для того, чтобы существование объекта с неразрешенными полями было невозможным, реализуем статические методы для проверки каждого поля. validate_teacher_id - проверяет, чтобы идентификатор учителя был положительным числом validate_non_empty_string - проверяет, чтобы строка была непустой validate_email - проверяет, чтобы email имел корректный формат validate_experience_years - проверяет, чтобы стаж работы был >=0 При создании объекта все поля проходят через методы валидации. Если хотя бы одно поле не проходит проверку, всплывает исключение ValueError. Каждый сеттер перед установкой нового значения вызывает соответствующий метод проверки.

--- Teacher.py
+++ Teacher.py
@@ -1,5 +1,21 @@
 class Teacher:
     def __init__(self, teacher_id: int, first_name: str, last_name: str, email: str, academic_degree: str, administrative_position: str, experience_years: int):
+
+        if not self.validate_teacher_id(teacher_id):
+            raise ValueError("Teacher ID должен быть положительным целым числом")
+        if not self.validate_non_empty_string(first_name):
+            raise ValueError("Имя должно быть непустой строкой")
+        if not self.validate_non_empty_string(last_name):
+            raise ValueError("Фамилия должна быть непустой строкой")
+        if not self.validate_email(email):
+            raise ValueError("Email имеет некорректный формат")
+        if not self.validate_non_empty_string(academic_degree):
+            raise ValueError("Ученая степень должна быть непустой строкой")
+        if not self.validate_non_empty_string(administrative_position):
+            raise ValueError("Административная должность должна быть непустой строкой")
+        if not self.validate_experience_years(experience_years):
+            raise ValueError("Стаж должен быть целым числом >= 0")
+
         self.__teacher_id = teacher_id
         self.__first_name = first_name
         self.__last_name = last_name
@@ -7,6 +23,23 @@ class Teacher:
         self.__academic_degree = academic_degree
         self.__administrative_position = administrative_position
         self.__experience_years = experience_years
+
+    # 4 основных статических методов валидации идентификатора, имени и тд.
+    @staticmethod
+    def validate_teacher_id(teacher_id: int) -> bool:
+        return isinstance(teacher_id, int) and teacher_id > 0
+
+    @staticmethod
+    def validate_email(email: str) -> bool:
+        return isinstance(email, str) and "@" in email and "." in email
+
+    @staticmethod
+    def validate_non_empty_string(value):
+        return isinstance(value, str) and len(value.strip()) > 0
+
+    @staticmethod
+    def validate_experience_years(experience_years: int) -> bool:
+        return isinstance(experience_years, int) and experience_years >= 0
 
     # getters (чтение значений)
     def get_teacher_id(self):
@@ -30,30 +63,48 @@ class Teacher:
     def get__experience_years(self):
         return self.__experience_years
 
-    # setters (изменение значений)
-    def set_teacher_id(self, teacher_id:int):
-        self.__teacher_id = teacher_id
+    # setters (изменение значений) с валидацией
+    def set_teacher_id(self, teacher_id: int):
+        if self.validate_teacher_id(teacher_id):
+            self.__teacher_id = teacher_id
+        else:
+            raise ValueError("Invalid teacher id")
 
     def set_first_name(self, first_name: str):
-        self.__first_name = first_name
+        if self.validate_non_empty_string(first_name):
+            self.__first_name = first_name
+        else:
+            raise ValueError("Invalid first name")
 
-    def set_last_name(self, last_name:str):
-        self.__last_name = last_name
+    def set_last_name(self, last_name: str):
+        if self.validate_non_empty_string(last_name):
+            self.__last_name = last_name
+        else:
+            raise ValueError("Invalid last name")
 
     def set_email(self, email: str):
-        self.__email = email
+        if self.validate_email(email):
+            self.__email = email
+        else:
+            raise ValueError("Invalid email")
 
-    def set_academic_degree(self, academic_degree:str):
-        self.__academic_degree = academic_degree
+    def set_academic_degree(self, academic_degree: str):
+        if self.validate_non_empty_string(academic_degree):
+            self.__academic_degree = academic_degree
+        else:
+            raise ValueError("Invalid academic degree")
 
     def set_administrative_position(self, administrative_position: str):
-        self.__administrative_position = administrative_position
+        if self.validate_non_empty_string(administrative_position):
+            self.__administrative_position = administrative_position
+        else:
+            raise ValueError("Invalid administrative position")
 
     def set_experience_years(self, experience_years: int):
-        if experience_years >= 0:
+        if self.validate_experience_years(experience_years):
             self.__experience_years = experience_years
         else:
-            raise ValueError("Стаж должен быть больше или равен 0")
+            raise ValueError("Invalid experience years")
 
     def __str__(self):
         return (f"Teacher_ID: {self.__teacher_id}"
