c46d6687c92dc81ad12d8f8be98ab5c49c06e0c0
DariaDyak <144004029+DariaDyak@users.noreply.github.com>
2025-09-09 22:54:41 +0300

    Задание 5. Для того, чтобы исключить повтор кода, можно реализовать метод валидации всех строковых полей Реализованы методы: validate_teacher_id (статический) - проверяет идентификатор учителя validate_email (статический) - проверяет почту учителя на корректность validate_string (универсальный) - проверяет строковые поля, а именно: имя, фамилию, email, ученую степень, должность. Аргумент field_name позволяет отобразить поле, которое выдаст ошибку при валидации validate_experience_years (статический) - проверяет стаж работы учителя

--- Teacher.py
+++ Teacher.py
@@ -1,45 +1,37 @@
 class Teacher:
     def __init__(self, teacher_id: int, first_name: str, last_name: str, email: str, academic_degree: str, administrative_position: str, experience_years: int):
 
-        if not self.validate_teacher_id(teacher_id):
-            raise ValueError("Teacher ID должен быть положительным целым числом")
-        if not self.validate_non_empty_string(first_name):
-            raise ValueError("Имя должно быть непустой строкой")
-        if not self.validate_non_empty_string(last_name):
-            raise ValueError("Фамилия должна быть непустой строкой")
-        if not self.validate_email(email):
-            raise ValueError("Email имеет некорректный формат")
-        if not self.validate_non_empty_string(academic_degree):
-            raise ValueError("Ученая степень должна быть непустой строкой")
-        if not self.validate_non_empty_string(administrative_position):
-            raise ValueError("Административная должность должна быть непустой строкой")
-        if not self.validate_experience_years(experience_years):
-            raise ValueError("Стаж должен быть целым числом >= 0")
+        self.set_teacher_id(teacher_id)
+        self.set_first_name(first_name)
+        self.set_last_name(last_name)
+        self.set_email(email)
+        self.set_academic_degree(academic_degree)
+        self.set_administrative_position(administrative_position)
+        self.set_experience_years(experience_years)
 
-        self.__teacher_id = teacher_id
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__email = email
-        self.__academic_degree = academic_degree
-        self.__administrative_position = administrative_position
-        self.__experience_years = experience_years
-
-    # 4 основных статических методов валидации идентификатора, имени и тд.
     @staticmethod
     def validate_teacher_id(teacher_id: int) -> bool:
-        return isinstance(teacher_id, int) and teacher_id > 0
+        if not isinstance(teacher_id, int) or teacher_id <= 0:
+            raise ValueError("teacher id is invalid")
+        return True
 
     @staticmethod
     def validate_email(email: str) -> bool:
-        return isinstance(email, str) and "@" in email and "." in email
+        if not isinstance(email, str) or "@" not in email or "." not in email:
+            raise ValueError("email is invalid")
+        return True
 
     @staticmethod
-    def validate_non_empty_string(value):
-        return isinstance(value, str) and len(value.strip()) > 0
+    def validate_string(value: str, field_name: str) -> bool:
+        if not isinstance(value, str) or len(value.strip()) == 0:
+            raise ValueError(f"{field_name} is invalid")
+        return True
 
     @staticmethod
     def validate_experience_years(experience_years: int) -> bool:
-        return isinstance(experience_years, int) and experience_years >= 0
+        if not isinstance(experience_years, int) or experience_years < 0:
+            raise ValueError("experience years is invalid")
+        return True
 
     # getters (чтение значений)
     def get_teacher_id(self):
@@ -65,46 +57,32 @@ class Teacher:
 
     # setters (изменение значений) с валидацией
     def set_teacher_id(self, teacher_id: int):
-        if self.validate_teacher_id(teacher_id):
-            self.__teacher_id = teacher_id
-        else:
-            raise ValueError("Invalid teacher id")
+        self.validate_teacher_id(teacher_id)
+        self.__teacher_id = teacher_id
 
     def set_first_name(self, first_name: str):
-        if self.validate_non_empty_string(first_name):
-            self.__first_name = first_name
-        else:
-            raise ValueError("Invalid first name")
+        self.validate_string(first_name, "First name")
+        self.__first_name = first_name
 
     def set_last_name(self, last_name: str):
-        if self.validate_non_empty_string(last_name):
-            self.__last_name = last_name
-        else:
-            raise ValueError("Invalid last name")
+        self.validate_string(last_name, "Last name")
+        self.__last_name = last_name
 
     def set_email(self, email: str):
-        if self.validate_email(email):
-            self.__email = email
-        else:
-            raise ValueError("Invalid email")
+        self.validate_email(email)
+        self.__email = email
 
     def set_academic_degree(self, academic_degree: str):
-        if self.validate_non_empty_string(academic_degree):
-            self.__academic_degree = academic_degree
-        else:
-            raise ValueError("Invalid academic degree")
+        self.validate_string(academic_degree, "Academic degree")
+        self.__academic_degree = academic_degree
 
     def set_administrative_position(self, administrative_position: str):
-        if self.validate_non_empty_string(administrative_position):
-            self.__administrative_position = administrative_position
-        else:
-            raise ValueError("Invalid administrative position")
+        self.validate_string(administrative_position, "Administrative position")
+        self.__administrative_position = administrative_position
 
     def set_experience_years(self, experience_years: int):
-        if self.validate_experience_years(experience_years):
-            self.__experience_years = experience_years
-        else:
-            raise ValueError("Invalid experience years")
+        self.validate_experience_years(experience_years)
+        self.__experience_years = experience_years
 
     def __str__(self):
         return (f"Teacher_ID: {self.__teacher_id}"
